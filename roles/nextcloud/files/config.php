8f5570b4072cc777c5eff9ad4cf994387f837738
Nither <xu_junkai@foxmail.com>
2018-10-18 10:33:12 +0800

    123

--- roles/nextcloud/files/config.php
+++ roles/nextcloud/files/config.php
@@ -1,6 +1,5 @@
 <?php
 $CONFIG = array (
-  'instanceid' => 'ocsxfea3dd4z',
   'memcache.locking' => '\\OC\\Memcache\\Redis',
   'memcache.local' => '\\OC\\Memcache\\Redis',
   'redis' =>
@@ -8,5 +7,6 @@ $CONFIG = array (
         'host' => 'localhost',
         'port' => 6379,
   ),
-
+  'mail_smtpmode' => 'smtp',
+  'mail_smtpsecure' => 'ssl',
 );
